85fe3dd9eb57ef41a65d0616c1ad05cc4507560e
Nesqui <nesqui@bk.ru>
2019-10-26 16:22:26 +0500

    files

---
+++ src/views/TopMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="the-nav d-flex mb-1 py-2 justify-content-center justify-content-around">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <a @click="logout">Выйти</a>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch(`logout`);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.the-nav {
+  border-bottom: 1px solid black;
+}
+</style>
