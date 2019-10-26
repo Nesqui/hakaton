7bafb9de01bd5a664e262a4d8e286a263dc995f0
Nesqui <nesqui@bk.ru>
2019-10-26 14:49:22 +0500

    files

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAVFDvODQE3Kf7PcIj9C1HEjg3mBAhhusA",
+    authDomain: "hakaton-77726.firebaseapp.com",
+    databaseURL: "https://hakaton-77726.firebaseio.com",
+    projectId: "hakaton-77726",
+    storageBucket: "hakaton-77726.appspot.com",
+    messagingSenderId: "202525258825",
+    appId: "1:202525258825:web:e9339c0816d6cadd067b98"
+};
+
+firebase.initializeApp(config);
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div class="container">
+          <div class="row d-flex justify-content-center">
+              <div class="col"></div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
